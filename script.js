a8967ba28424120bac12badba44ea65eacf9aae7
Fernando Saito <fernandosaito@hotmail.com.br>
2022-10-18 14:24:28 -0400

    inicial

---
+++ script.js
@@ -0,0 +1,20 @@
+function alterar(titulo) {
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("campo").value = titulo
+
+    return titulo
+}
+
+
+function trocarImagem(filename, animalName) {
+    document.querySelector('.imagem').setAttribute("src", 'Images/'+filename);
+    document.querySelector('.imagem').setAttribute('data-animal', animalName);
+
+}
+
+function pegarAnimal() {
+    let animal = document.querySelector('.imagem').getAttribute('data-animal');
+    alert(animal) //
+}
+
+
